d7cc874289e5484828f79b3fba61a64bcf2ed195
Mohd-Saad30 <saadmohd835@gmail.com>
2025-09-03 15:37:03 +0530

    Enhance styling and layout for improved responsiveness

--- index.js
+++ index.js
@@ -3,20 +3,13 @@ document.addEventListener("DOMContentLoaded", function() {
     document.getElementById("headPara").textContent = "";
 
     let i = 0;
-    const speed = 100; // The delay in milliseconds between each character
+    const speed = 100; 
     function typeWriter() {
         if (i < text.length) {
             document.getElementById("headPara").textContent += text.charAt(i);
             i++;
             setTimeout(typeWriter, speed);
         } 
-        // else {
-        //     setTimeout(function() {
-        //         document.getElementById("headPara").textContent = "";
-        //         i = 0;
-        //         typeWriter();
-        //     }, 2000); // Optional delay before restarting
-        // }
     }
     const colorGenerator=()=>{
         const hex="0123456789ABCDEF";
