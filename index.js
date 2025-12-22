6cea9576915d5e92164ba9fd59e1f78d82c792c4
Mohd-Saad30 <saadmohd835@gmail.com>
2025-12-22 23:57:49 +0530

    Enhance portfolio layout.

--- index.js
+++ index.js
@@ -1,32 +1,41 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const text = document.getElementById("headPara").textContent;
-    document.getElementById("headPara").textContent = "";
+document.addEventListener("DOMContentLoaded", function () {
+  const elements = [
+    { id: "line1", text: "Hi, I am" },
+    { id: "line2", text: "MOHD SAAD" },
+    { id: "line3", text: "Frontend Developer" },
+    {
+      id: "line4",
+      text: "Frontend Engineering for web and exploring backend too. Currently a Computer Science student at Jamia Hamdard University.",
+    },
+  ];
 
-    let i = 0;
-    const speed = 100; 
-    function typeWriter() {
-        if (i < text.length) {
-            document.getElementById("headPara").textContent += text.charAt(i);
-            i++;
-            setTimeout(typeWriter, speed);
-        } 
+  elements.forEach((item) => {
+    document.getElementById(item.id).textContent = "";
+  });
+
+  let elementIndex = 0;
+  let charIndex = 0;
+  const typingSpeed = 100;
+  const lineDelay = 100;
+
+  function typeWriter() {
+    if (elementIndex < elements.length) {
+      let currentItem = elements[elementIndex];
+
+      let currentElement = document.getElementById(currentItem.id);
+
+      if (charIndex < currentItem.text.length) {
+        currentElement.textContent += currentItem.text.charAt(charIndex);
+        charIndex++;
+        setTimeout(typeWriter, typingSpeed);
+      } else {
+        
+        elementIndex++;
+        charIndex = 0;
+        setTimeout(typeWriter, lineDelay);
+      }
     }
-    const colorGenerator=()=>{
-        const hex="0123456789ABCDEF";
-        let word='#'
-        for (let i =0 ;i<6;i++){
-         let random=hex[Math.floor(Math.random()*16)];
-         word+=random;
+  }
 
-        }
-        return word;  
-    };
-
-    setInterval(() => {
-    document.getElementById('textSaad').style.color=colorGenerator()
-    }, 1000);
-
-
-    typeWriter();
+  typeWriter();
 });
-
